310764619c0f656d0a620a7bd8d9a8b2e50a774f
brookswi <brookswi@oregonstate.edu>
2017-11-30 13:55:35 -0600

    Update handlePageEvents.js

--- handlePageEvents.js
+++ handlePageEvents.js
@@ -1,46 +1,48 @@
-    document.querySelector('#sign-in').addEventListener('click', function(e) {
+document.querySelector('#sign-in').addEventListener('click', function(e) {
       e.preventDefault();
       e.stopPropagation();
       var email = document.querySelector('#email').value;
       var password = document.querySelector('#password').value
-   //   var credential = firebase.auth().EmailAuthProvider.credential(email, password); commented this out (Patrick)
       var currentUser = firebase.auth().currentUser;
-   //   firebase.auth().signIn(); //added this (DEM)
-     // firebase.auth.emailAuthProvider.credential(emailInput.value, passwordInput.value);//added this (DEM)  
-    //  firebase.auth.signInWithCredential(credential);//added this (DEM)
-      // Step 2
-      //  Get a credential with firebase.auth.emailAuthProvider.credential(emailInput.value, passwordInput.value)
-      //  If there is no current user, log in with auth.signInWithCredential(credential)
-      //  If there is a current user an it's anonymous, atttempt to link the new user with firebase.auth().currentUser.link(credential)
-      //  The user link will fail if the user has already been created, so catch the error and sign in.
-        // Added this - Patrick, commented out lines 6, 10, 11
-        // Sign in user
-        var signedInUser = firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-             // ...
-            // If signed in user is null, create account
-            if(errorCode == "auth/user-not-found"){
-                signedInUser = firebase.auth().createUserWithEmailAndPassword(email, password);
-                var user = firebase.auth().currentUser;
-                user.sendEmailVerification().then(function() {
-                        // Email sent.
-                        }).catch(function(error) {
-                        // An error happened.
-                        console.log(error.message);
-                });
-            }
-            else console.log(errorMessage);
-        });
-        // If user sign in successful, redirect to search.
-        if(signedInUser){
-            window.location.href = 'https://silencetheecho.github.io/SilenceTheEcho/search'; 
-        } 
-            // end add by patrick
-    });
-    document.querySelector('#sign-out').addEventListener('click', function(e) {
-      e.preventDefault();
-      e.stopPropagation();
-      firebase.auth().signOut();
-    });
+   
+      // Sign in user
+      var signedInUser = firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+       
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+            
+      // If signed in user is null, create account
+      if(errorCode == "auth/user-not-found"){
+          signedInUser = firebase.auth().createUserWithEmailAndPassword(email, password);
+          var user = firebase.auth().currentUser;
+          user.sendEmailVerification().then(function() {
+           // Email sent.
+          }).catch(function(error) {
+            // An error happened.
+            console.log(error.message);
+            });
+      } else console.log(errorMessage);
+          
+      // If user sign in successful, redirect to search.
+      //window.location.href = 'https://silencetheecho.github.io/SilenceTheEcho/search';
+      if (firebase.auth().currentUser)
+      {
+        var signedIn = document.createElement("p");
+        signedIn.id = "signedIn";
+        signedIn.textContent = "You are signed in as " + firebase.auth().currentUser["email"];
+        document.getElementById("password").appendChild(signedIn);
+      }
+      else
+      {
+        var signedIn = document.getElementById("signedIn");
+        if (signedIn)
+            document.getElementById("password").removeChild(signedIn);
+      }
+});
+        
+document.querySelector('#sign-out').addEventListener('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    firebase.auth().signOut();
+});
