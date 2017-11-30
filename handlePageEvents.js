7a88896cb19fd267550282763fd7748060dc226e
Patrick Mullaney <mullanep@oregonstate.edu>
2017-11-30 18:25:23 -0500

    Update handlePageEvents.js

--- handlePageEvents.js
+++ handlePageEvents.js
@@ -14,14 +14,15 @@ document.querySelector('#sign-in').addEventListener('click', function(e) {
             
             // If signed in user is null, create account
             if(errorCode == "auth/user-not-found"){
-                  signedInUser = firebase.auth().createUserWithEmailAndPassword(email, password);
-                  //var user = firebase.auth().currentUser;
-                  signedInUser.sendEmailVerification().then(function() {
+                  signedInUser = firebase.auth().createUserWithEmailAndPassword(email, password).then(function(){
+                        //var user = firebase.auth().currentUser;
+                        signedInUser.sendEmailVerification().then(function() {
                         // Email sent.
-                  }).catch(function(error) {
-                    // An error happened.
-                    console.log(error.message);
-                    });
+                        }).catch(function(error) {
+                              // An error happened.
+                        console.log(error.message);
+                        });
+                  });
             } else console.log(errorMessage);
       });
 });
