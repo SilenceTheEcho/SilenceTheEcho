28b08dbb7c2d022793d06ba82e8a8eb10e53f62f
chfreem <freemche@oregonstate.edu>
2017-11-30 14:56:13 +0300

    Create initializeFirebase.js

---
+++ initializeFirebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAbBnEsH-88WhhZqjG0xczXXriqvYRA_y4",
+    authDomain: "silencetheecho-efa5e.firebaseapp.com",
+    databaseURL: "https://silencetheecho-efa5e.firebaseio.com",
+    projectId: "silencetheecho-efa5e",
+    storageBucket: "silencetheecho-efa5e.appspot.com",
+    messagingSenderId: "765642044089"
+  };
+  firebase.initializeApp(config);
