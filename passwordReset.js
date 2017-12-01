b242a44a34521b9a042ae924b9c14ac8e5354751
Patrick Mullaney <mullanep@oregonstate.edu>
2017-11-30 19:37:32 -0500

    Update passwordReset.js

--- passwordReset.js
+++ passwordReset.js
@@ -1,8 +1,23 @@
 var auth = firebase.auth();
-var emailAddress = "user@example.com";
+//var emailAddress = "user@example.com";
 
-auth.sendPasswordResetEmail(emailAddress).then(function() {
-  // Email sent.
-}).catch(function(error) {
-  // An error happened.
+document.querySelector('#submit').addEventListener('click', function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      var emailAddress = document.querySelector('#email').value;
+      var currentUser = firebase.auth().currentUser;
+   
+      // Send password reset
+      firebase.auth().sendPasswordResetEmail(emailAddress).catch(function(error) {
+       
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            
+            // If signed in user is null, create account
+            if(errorCode == "auth/user-not-found"){
+                 alert(errorMessage);
+            } 
+            else console.log(errorMessage);
+      });
 });
